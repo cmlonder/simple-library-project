badd9cec0f878165de873426504481931d7dd7bd
Cemal <cemalonder1@gmail.com>
2019-01-12 08:53:07 +0300

    initial commit

---
+++ src/main/java/com/cemalonder/tutorials/simplelibraryproject/SimpleLibraryProjectApplication.java
@@ -0,0 +1,14 @@
+package com.cemalonder.tutorials.simplelibraryproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleLibraryProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleLibraryProjectApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/cemalonder/tutorials/simplelibraryproject/SimpleLibraryProjectApplicationTests.java
@@ -0,0 +1,17 @@
+package com.cemalonder.tutorials.simplelibraryproject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SimpleLibraryProjectApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
