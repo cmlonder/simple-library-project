48335225a4184fc669f927c98fc635cbe8cd42f6
Cemal <cemalonder1@gmail.com>
2019-01-15 00:20:27 +0300

    google java code style is added

--- src/main/java/com/cemalonder/tutorials/simplelibraryproject/SimpleLibraryProjectApplication.java
+++ src/main/java/com/cemalonder/tutorials/simplelibraryproject/SimpleLibraryProjectApplication.java
@@ -6,9 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SimpleLibraryProjectApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(SimpleLibraryProjectApplication.class, args);
-    }
-
+  public static void main(String[] args) {
+    SpringApplication.run(SimpleLibraryProjectApplication.class, args);
+  }
 }
-
